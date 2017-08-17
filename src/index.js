f97c0a03153dc24955020c2a79b62dce3bde5a26
belfz <marcinbar1@gmail.com>
2017-08-17 22:54:31 +0200

    removes needless console.log

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ const wasm = require('./main.rs');
 
 wasm.initialize({noExitRuntime: true}).then(module => {
   const add = module.cwrap('add', 'number', ['number', 'number']);
-
-  console.log('Calling rust functions from JS');
   console.log(add(40, 2));
 });
